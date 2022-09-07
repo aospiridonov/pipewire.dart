e2a8b0fd493391c4400b2fc72dd28192fe60334e
aospiridonov <aospiridonov@gmail.com>
2022-09-07 17:13:12 +0300

    fix

--- example/main.dart
+++ example/main.dart
@@ -2,4 +2,5 @@ import 'package:pipewire/pipewire.dart';
 
 void main(List<String> args) async {
   Pipewire pw = Pipewire();
+  print('version: ${pw.version}');
 }
--- lib/src/pipewire.dart
+++ lib/src/pipewire.dart
@@ -2,15 +2,12 @@ library pipewire;
 
 import 'package:ffi/ffi.dart';
 import 'dart:ffi' as ffi;
-import 'package:ffi/src/utf8.dart';
-
 import './pipewire/generated_bindings.dart' as pw;
 
 class Pipewire {
   late final pw.Pipewire _ffiLibBindings;
 
   Pipewire() {
-    //final path = '/usr/lib/pipewire-0.3/libpipewire.so';
     final path = 'libpipewire-0.3.so';
     final dynamicLibrary = ffi.DynamicLibrary.open(path);
     _ffiLibBindings = pw.Pipewire(dynamicLibrary);
@@ -18,11 +15,10 @@ class Pipewire {
     ffi.Pointer<ffi.Pointer<ffi.Pointer<ffi.Char>>> argv =
         calloc<ffi.Pointer<ffi.Pointer<ffi.Char>>>();
     _ffiLibBindings.pw_init(argc, argv);
-    final pointerVersion =
-        _ffiLibBindings.pw_get_library_version().cast<Utf8>();
-    print(pointerVersion.toDartString());
-
     calloc.free(argc);
     calloc.free(argv);
   }
+
+  String get version =>
+      _ffiLibBindings.pw_get_library_version().cast<Utf8>().toDartString();
 }
